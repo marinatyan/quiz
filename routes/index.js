f1329f7bafca2b197391f058e5aaccc084dc4b0a
Marina Tyan <marina.tyan@alumnos.upm.es>
2016-04-26 18:41:35 +0200

    Crear página de créditos

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,7 @@ router.get('/', function(req, res) {
 router.get('/question', quizController.question);
 router.get('/check', quizController.check);
 
+router.get('/author', function(req, res) {
+  res.render('author', {title: 'Autores del Quiz'});
+});
 module.exports = router;
