327f7d971f3980a5bf818ccc72e8c7c7747e2d0c
Marina Tyan <marina.tyan@alumnos.upm.es>
2016-04-27 10:45:49 +0200

    Base de datos: sequelize.js y SQLite

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,19 +1,36 @@
+var models = require('../models');
 //GET /question
 exports.question = function (req, res, next) {
-   var answer = req.query.answer || "";
-   res.render('quizzes/question', {question: 'Capital de Italia',
+   models
+   .Quiz
+   .findOne() //Busca l aprimera pregunta
+   .then (function (quiz) {
+     if (quiz) {
+       var answer = req.query.answer || "";
+       res.render('quizzes/question', {question: quiz.question,
                                    answer: answer});
+     }
+     else {
+       throw new Error ('No hay preguntas en la BBDD.');
+     }
+    }).catch(function(error) {next(error);});
 };
 
 //GET /check
 exports.check = function (req, res, next) {
-   var answer = req.query.answer || "";
-   var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta';
-   res.render('quizzes/result', {result: result,
-				 answer: answer});
+   models
+   .Quiz
+   .findOne() //BUsca la primera pregunta
+   .then (function(quiz) {
+      if (quiz) {
+        var answer = req.query.answer || "";
+        var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
+        res.render('quizzes/result', {result: result,
+				      answer: answer});
+      }
+      else {
+        throw new Error ('No hay preguntas en la BBDD.');
+      }
+    }).catch(function(error) { next(error); });
 };
 
-//GET /author
-//exports.author = function (req, res, next) {
-  // res.render('author', {title:'Autores del Quiz'});
-//};
