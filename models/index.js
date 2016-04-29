c097a4e01759a58cf277c2558f31f7b8fb6ef52f
Marina Tyan <marina.tyan@alumnos.upm.es>
2016-04-29 10:55:01 +0200

    Despliegue DB en Heroku

--- models/index.js
+++ models/index.js
@@ -4,17 +4,30 @@ var path = require('path');
 
 var Sequelize = require('sequelize');
 
+//Usar BBDD SQLite
+// DATABASE_URL = sqlite:///
+// DATABASE_STORAGE = quiz.sqlite
+// Usar BBDD Postgres:
+// DATABASE_URL = postgres://user:passwd@host:port/database
+
+var url, storage;
+
+if(!process.env.DATABASE_URL) {
+url = "sqlite:///";
+storage= "quiz.sqlite";
+} else {
+url = process.env.DATABASE_URL;
+storage = process.env.DATABASE_STORAGE || "";
+}
 //Usar BBDD SQLite:
-var sequelize = new Sequelize(null, null, null,
-                      { dialect: "sqlite", storage: "quiz.sqlite" });
+var sequelize = new Sequelize(url,
+                      { storage: storage, omitNull:true });
 
 //Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize
-.sync()
-.then(function() { //sync() crea la tabla quiz
+sequelize.sync().then(function() { //sync() crea la tabla quiz
   return
     Quiz
     .count()
