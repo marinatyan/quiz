16209bc15fb799485590028d6a0016918444145e
Marina Tyan <marina.tyan@alumnos.upm.es>
2016-05-01 13:14:40 +0200

    Cajetín de busqueda. BBDD arreglada

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,13 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
-router.get('/question', quizController.question);
-router.get('/check', quizController.check);
+//router.get('/question', quizController.question);
+//router.get('/check', quizController.check);
+
+//definición de rutas de /quizzes
+router.get('/quizzes', quizController.index);
+router.get('/quizzes/:quizId(\\d+)', quizController.show);
+router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 
 router.get('/author', function(req, res) {
   res.render('author', {title: 'Autores del Quiz'});
