8b41f75769b9341a8342291e64f29b671152ed1f
mariabguerrero <maria.bguerrero@alumnos.upm.es>
2016-05-10 12:36:15 +0200

    Borrar preguntas

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
 router.put('/quizzes/:quizId(\\d+)',       quizController.update);
 router.get('/quizzes/new',                 quizController.new);
 router.post('/quizzes',                    quizController.create);
-
+router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
 router.get('/author', function(req, res) {
   res.render('author', {title: 'Autores del Quiz'});
 });
