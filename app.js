09a656833a2b2b45ab8236b293ab100f2ca20641
mariabguerrero <maria.bguerrero@alumnos.upm.es>
2016-05-11 20:33:59 +0200

    Autenticación y sesión

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.set('view engine', 'ejs');
 app.use(partials());
 app.use(flash());
 
+
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -30,6 +31,16 @@ app.use(session({secret: "Quiz 2016",
                 saveUninitialized: true}));
 app.use(methodOverride('_method', {methods: ["POST", "GET"]}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Helper dinamico:
+app.use(function(req, res, next) {
+
+   // Hacer visible req.session en las vistas
+   res.locals.session = req.session;
+
+   next();
+});
+
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var commentController = require('../controllers/comment_controller');
 var quizController = require('../controllers/quiz_controller');
 var userController = require('../controllers/user_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -14,6 +15,12 @@ router.get('/', function(req, res) {
 
 router.param('quizId', quizController.load); //autoload: quizId
 router.param('userId', userController.load);  // autoload :userId
+
+// Definición de rutas de sesion
+router.get('/session',    sessionController.new);     // formulario login
+router.post('/session',   sessionController.create);  // crear sesión
+router.delete('/session', sessionController.destroy); // destruir sesión
+
 
 // Definición de rutas de cuenta
 router.get('/users',                    userController.index);   // listado usuarios
